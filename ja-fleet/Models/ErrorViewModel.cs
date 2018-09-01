6c98e8dd12ad776a70e44bde6db6b63d96929ce5
noobow <skywind.hk34@gmail.com>
2018-09-01 10:01:09 +0900

    追加

---
+++ ja-fleet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ja_fleet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
