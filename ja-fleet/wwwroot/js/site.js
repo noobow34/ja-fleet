1679604c11f293bf5bebadeb73694402699404f1
noobow <skywind.hk34@gmail.com>
2018-09-16 08:49:03 +0900

    excel読み込み暫定対応

--- ja-fleet/wwwroot/js/site.js
+++ ja-fleet/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function setupTable(tableName,url)
+{
+            $.extend( $.fn.dataTable.defaults, {
+                language: {
+                    url: "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json"
+                }
+            });
+            $(tableName).DataTable({
+                ajax: {url:url,
+                        dataSrc: ''},
+                columns: [
+                    {data: 'airline'},
+                    {data: 'type'},
+                    {data: 'registrationNumber'},
+                    {data: 'serialNumber'},
+                    {data: 'registrationDate'},
+                    {data: 'status'},
+                    {data: 'wifi'},
+                    {data: 'remarks'},
+                ],
+                "paging":   false,
+                "ordering": false,
+                "columnDefs": [{
+                    "targets": '_all',
+                    "className": 'dt-center'
+                }]
+
+            });
+}
