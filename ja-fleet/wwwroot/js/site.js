eaf50ec4ca8729ab28d8aa8913305fc556a4c59d
noobow <skywind.hk34@gmail.com>
2018-09-27 22:15:18 +0900

    rinを適用

--- ja-fleet/wwwroot/js/site.js
+++ ja-fleet/wwwroot/js/site.js
@@ -1,9 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-function setupTable(tableName,url)
+﻿function setupTable(tableName,url)
 {
             $.extend( $.fn.dataTable.defaults, {
                 language: {
