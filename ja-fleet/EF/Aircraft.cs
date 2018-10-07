4ccfe9b44a5079b09f09e0aa626b032c16de09f0
noobow <skywind.hk34@gmail.com>
2018-10-07 20:38:09 +0900

    ドロップダウンの空白を追加、作成日時・更新日時を表示

--- ja-fleet/EF/Aircraft.cs
+++ ja-fleet/EF/Aircraft.cs
@@ -4,6 +4,8 @@ namespace jafleet.EF
 {
     public partial class Aircraft
     {
+        [Display(Name = "航空会社")]
+        public string Airline { get; set; }
         [Display(Name = "機体記号")]
         public string RegistrationNumber { get; set; }
         [Display(Name = "型式")]
@@ -18,9 +20,9 @@ namespace jafleet.EF
         public string Remarks { get; set; }
         [Display(Name = "製造番号")]
         public string SerialNumber { get; set; }
+        [Display(Name = "作成日時")]
         public string UpdateTime { get; set; }
+        [Display(Name = "更新日時")]
         public string CreationTime { get; set; }
-        [Display(Name = "航空会社")]
-        public string Airline { get; set; }
     }
 }
