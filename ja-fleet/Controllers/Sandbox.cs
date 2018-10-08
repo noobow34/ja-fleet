4b75246f6ed545e249728a35974fc39ce7a6dc6d
noobow <skywind.hk34@gmail.com>
2018-10-08 11:00:01 +0900

    エラー画面作成中

---
+++ ja-fleet/Controllers/Sandbox.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace jafleet.Controllers
+{
+    public class Sandbox : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ex(){
+            throw new System.Exception();
+        }
+
+    }
+}
