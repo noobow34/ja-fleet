782e676fab7bcfad8db6f2a17dd493b84925bbbe
noobow34 <skywind.hk34@gmail.com>
2018-11-10 15:17:14 +0900

    機械的にjetphotosの最新の写真を取得するように変更

--- ja-fleet/Controllers/Aircraft.cs
+++ ja-fleet/Controllers/Aircraft.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using jafleet.Models;
 using jafleet.EF;
 using jafleet.Manager;
+using AngleSharp.Dom.Html;
+using System.Net.Http;
+using AngleSharp.Parser.Html;
+using System;
 
 namespace jafleet.Controllers
 {
@@ -72,6 +76,39 @@ namespace jafleet.Controllers
             ViewData["TableId"] = id;
             ViewData["api"] = "/api/type/" + id;
             return View("~/Views/Aircraft/index.cshtml");
+        }
+
+        public async System.Threading.Tasks.Task<IActionResult> Photo(string id)
+        {
+            string jetphotoUrl = string.Format("https://www.jetphotos.com/showphotos.php?keywords-type=reg&keywords={0}&search-type=Advanced&keywords-contain=0&sort-order=2", id);
+            string redirectUrl = string.Empty;
+
+            using (var context = new jafleetContext())
+            {
+                redirectUrl = context.Aircraft.Where(p => p.RegistrationNumber == id.ToUpper()).FirstOrDefault()?.LinkUrl;
+            }
+
+            if(redirectUrl == null){
+                var parser = new HtmlParser();
+                HttpClient client = new HttpClient();
+                var htmlDocument = parser.Parse(await client.GetStringAsync(jetphotoUrl));
+                var photos = htmlDocument.GetElementsByClassName("result__photoLink");
+                if (photos.Length != 0)
+                {
+                    string newestPhotoLink = photos[0].GetAttribute("href");
+                    redirectUrl = "https://www.jetphotos.com" + newestPhotoLink;
+                    return Redirect(redirectUrl);
+                }
+                else
+                {
+                    return Redirect(jetphotoUrl);
+                }
+            }
+            else
+            {
+                return Redirect(redirectUrl);
+            }
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
