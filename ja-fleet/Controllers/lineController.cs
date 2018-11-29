85a7f9efacfe6d6f09034c29b7209b9b1f0dcb15
noobow34 <skywind.hk34@gmail.com>
2018-11-30 00:06:33 +0900

    #34

---
+++ ja-fleet/Controllers/lineController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ja_fleet.Controllers
+{
+    public class lineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("https://line.me/R/ti/p/BTy1CuBCzF");
+        }
+    }
+}
