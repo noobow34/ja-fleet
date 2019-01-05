0ab95ef1df5bbee2d92dcd070c927a8b3ad7c313
noobow34 <skywind.hk34@gmail.com>
2019-01-06 07:59:46 +0900

    lineリンクログ出力

--- ja-fleet/Controllers/lineController.cs
+++ ja-fleet/Controllers/lineController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using jafleet.Constants;
+using jafleet.EF;
+using jafleet.Util;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
 
 namespace ja_fleet.Controllers
 {
@@ -6,6 +12,20 @@ namespace ja_fleet.Controllers
     {
         public IActionResult Index()
         {
+            Task.Run(() => {
+                var lineLinklog = new Log
+                {
+                    LogType = LogType.LINE_LINK,
+                    UserId = CookieUtil.IsAdmin(HttpContext).ToString(),
+                    LogDate = DateTime.Now
+                };
+                using (var context = new jafleetContext())
+                {
+                    context.Log.Add(lineLinklog);
+                    context.SaveChanges();
+                }
+
+            });
             return Redirect("https://line.me/R/ti/p/BTy1CuBCzF");
         }
     }
