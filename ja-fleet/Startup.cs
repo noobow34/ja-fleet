f60468c8302dd69a75d47c2431b7692286fa79d8
noobow34 <skywind.hk34@gmail.com>
2019-01-31 23:07:27 +0900

    Compatibilityを変更

--- ja-fleet/Startup.cs
+++ ja-fleet/Startup.cs
@@ -39,7 +39,7 @@ namespace jafleet
 
             services.AddDbContext<jafleetContext>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
