36986712a4ef53721d3ce483530517a9e3600584
noobow34 <skywind.hk34@gmail.com>
2019-02-11 14:14:40 +0900

    Message記録

--- ja-fleet/Controllers/MessageController.cs
+++ ja-fleet/Controllers/MessageController.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.Configuration;
 using jafleet.Util;
 using Noobow.Commons.Utils;
 using jafleet.Manager;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace jafleet.Controllers
 {
@@ -13,11 +16,13 @@ namespace jafleet.Controllers
 
         private readonly jafleetContext _context;
         private readonly IConfiguration _configuration;
+        private readonly IServiceScopeFactory _services;
 
-        public MessageController(jafleetContext context, IConfiguration configuration)
+        public MessageController(jafleetContext context, IConfiguration configuration, IServiceScopeFactory serviceScopeFactory)
         {
             _context = context;
             _configuration = configuration;
+            _services = serviceScopeFactory;
         }
 
         public IActionResult Index()
@@ -31,6 +36,24 @@ namespace jafleet.Controllers
                 $"名前：{model.Name}\n" +
                 $"返信先：{model.Replay}\n" +
                 $"{model.Message}",HttpClientManager.GetInstance());
+            Task.Run(() => {
+                using (var serviceScope = _services.CreateScope())
+                {
+                    using (var context = serviceScope.ServiceProvider.GetService<jafleetContext>())
+                    {
+                        var m = new Message
+                        {
+                            Sender = model.Name,
+                            MessageDetail = model.Message,
+                            ReplayTo = model.Replay,
+                            MessageType = Commons.Constants.MessageType.WEB,
+                            RecieveDate = DateTime.Now
+                        };
+                        context.Messages.Add(m);
+                        context.SaveChanges();
+                    }
+                }
+            });
             return Content("OK");
         }
 
