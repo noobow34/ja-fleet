92025088087764b710ba321d980768450441ea19
noobow34 <skywind.hk34@gmail.com>
2019-04-13 08:09:24 +0900

    死活間使用のURLを設定

---
+++ ja-fleet/Controllers/CheckController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ja_fleet.Controllers
+{
+    public class CheckController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("1");
+        }
+    }
+}
