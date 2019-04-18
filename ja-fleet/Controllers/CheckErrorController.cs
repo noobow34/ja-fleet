38c5fef1c5738e3d15a501b33d1a5fef42f73826
noobow34 <skywind.hk34@gmail.com>
2019-04-18 21:22:34 +0900

    404を返す

---
+++ ja-fleet/Controllers/CheckErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace jafleet.Controllers
+{
+    public class CheckErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new NotFoundResult();
+        }
+    }
+}
