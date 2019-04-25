21abe1f3f940354bd5a473df07ef34c55089136c
noobow34 <skywind.hk34@gmail.com>
2019-04-25 21:36:21 +0900

    メッセージを簡略化

--- ja-fleet/Models/AircraftModel.cs
+++ ja-fleet/Models/AircraftModel.cs
@@ -5,7 +5,7 @@ namespace jafleet.Models
 {
     public class AircraftModel:BaseModel
     {
-        [Display(Name = "2018/09以降の退役（抹消済）も表示")]
+        [Display(Name = "2018/09以降の抹消済も表示")]
         public Boolean IncludeRetire { get; set; }
     }
 }
