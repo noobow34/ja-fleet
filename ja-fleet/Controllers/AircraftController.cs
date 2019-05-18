79c876888e4e3b67dbfa0f09e21f4aa512efe968
noobow34 <skywind.hk34@gmail.com>
2019-05-18 13:09:27 +0900

    Jetphotosに写真が登録されたら、自動的にLINK_URLを削除するようにする

--- ja-fleet/Controllers/AircraftController.cs
+++ ja-fleet/Controllers/AircraftController.cs
@@ -5,10 +5,13 @@ using jafleet.Models;
 using jafleet.Commons.EF;
 using jafleet.Manager;
 using AngleSharp.Html.Parser;
-using System;
 using jafleet.Util;
 using Microsoft.EntityFrameworkCore;
 using jafleet.Commons.Constants;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Noobow.Commons.Utils;
+using System;
 
 namespace jafleet.Controllers
 {
@@ -16,10 +19,12 @@ namespace jafleet.Controllers
     {
 
         private readonly jafleetContext _context;
+        private readonly IServiceScopeFactory _services;
 
-        public AircraftController(jafleetContext context)
+        public AircraftController(jafleetContext context, IServiceScopeFactory serviceScopeFactory)
         {
             _context = context;
+            _services = serviceScopeFactory;
         }
 
         public IActionResult Index(AircraftModel model)
@@ -121,32 +126,29 @@ namespace jafleet.Controllers
         public async System.Threading.Tasks.Task<IActionResult> Photo(string id)
         {
             string jetphotoUrl = string.Format("https://www.jetphotos.com/showphotos.php?keywords-type=reg&keywords={0}&search-type=Advanced&keywords-contain=0&sort-order=2", id);
-            string redirectUrl = string.Empty;
-
-            redirectUrl = _context.Aircraft.AsNoTracking().Where(p => p.RegistrationNumber == id.ToUpper()).FirstOrDefault()?.LinkUrl;
-
-            if(redirectUrl == null){
-                //DBでリンク先が指定されていない場合
-                var parser = new HtmlParser();
-                var htmlDocument = parser.ParseDocument(await HttpClientManager.GetInstance().GetStringAsync(jetphotoUrl));
-                var photos = htmlDocument.GetElementsByClassName("result__photoLink");
-                if (photos.Length != 0)
+            var a = _context.Aircraft.Where(p => p.RegistrationNumber == id.ToUpper()).FirstOrDefault();
+            var parser = new HtmlParser();
+            var htmlDocument = parser.ParseDocument(await HttpClientManager.GetInstance().GetStringAsync(jetphotoUrl));
+            var photos = htmlDocument.GetElementsByClassName("result__photoLink");
+            if (photos.Length != 0)
+            {
+                //Jetphotosに写真があった場合
+                string newestPhotoLink = photos[0].GetAttribute("href");
+                if (!string.IsNullOrEmpty(a.LinkUrl))
                 {
-                    //Jetphotosに写真があった場合
-                    string newestPhotoLink = photos[0].GetAttribute("href");
-                    redirectUrl = "https://www.jetphotos.com" + newestPhotoLink;
-                    return Redirect(redirectUrl);
+                    //Jetphotosから取得できるのにDBにも登録されている場合は、DBから消す
+                    a.LinkUrl = null;
+                    a.ActualUpdateTime = DateTime.Now;
+                    _context.SaveChanges();
+                    LineUtil.PushMe($"{id}のLinkUrlを削除じました", HttpClientManager.GetInstance());
                 }
-                else
-                {
-                    //Jetphotosに写真がなかった場合
-                    return Redirect("/nophoto.html");
-                }
+
+                return Redirect($"https://www.jetphotos.com{newestPhotoLink}");
             }
             else
             {
-                //DBでリンク先が指定されていない場合
-                return Redirect(redirectUrl);
+                //Jetphotosに写真がなかった場合
+                return Redirect(a?.LinkUrl ?? "/nophoto.html");
             }
 
         }
