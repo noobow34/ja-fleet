2060850e203a881c4367fd4c2b19b899e483e001
noobow34 <skywind.hk34@gmail.com>
2020-01-29 22:54:00 +0900

    保存済検索条件、戻るボタン改善

--- ja-fleet/wwwroot/js/site.js
+++ ja-fleet/wwwroot/js/site.js
@@ -53,3 +53,13 @@ const ZGB = function() {
     for (i = 0; i < s.length; i++)r += String.fromCharCode((s.charCodeAt(i) + 21) % 93 + 33);
     return r;
 }
+
+const getParam = function(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
