763eeff616119e854966d68536fa69ba0e47570c
noobow34 <skywind.hk34@gmail.com>
2020-04-19 20:18:52 +0900

    favicon exclude

--- ja-fleet/Classes/LoggingMiddleware.cs
+++ ja-fleet/Classes/LoggingMiddleware.cs
@@ -17,7 +17,7 @@ namespace jafleet.Classes
         private readonly RequestDelegate _next;
         private readonly IServiceScopeFactory _services;
 
-        private static string[] EXCLUDE_LIST = new string[] { ".CSS", ".JS", ".PNG",".JPG", ".JPEG", ".GIF" };
+        private static string[] EXCLUDE_LIST = new string[] { ".CSS", ".JS", ".PNG",".JPG", ".JPEG", ".GIF", ".ICO" };
 
         public LoggingMiddleware(RequestDelegate next, IServiceScopeFactory serviceScopeFactory)
         {
