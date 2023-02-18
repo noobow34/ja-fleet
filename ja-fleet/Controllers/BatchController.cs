f74a28dc4f4d011b2ed5f3fcf441a673ae8b603b
noobow34 <skywind.hk34@gmail.com>
2023-02-18 16:06:51 +0900

    using削除でReleaseビルドができなくなっていた

--- ja-fleet/Controllers/BatchController.cs
+++ ja-fleet/Controllers/BatchController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using jafleet.Commons.Constants;
 using jafleet.Commons.EF;
 using jafleet.Manager;
 using jafleet.Util;
