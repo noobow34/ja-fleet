9bf9a6f6f779a220251be7e78025cf287aaa92d9
Hiroaki KAWASHIMA <skywind.hk34@gmail.com>
2025-02-19 07:49:55 +0900

    qcuiklinkをAuthorize

---
+++ ja-fleet/Controllers/qlController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jafleet.Controllers
+{
+    [Authorize]
+    public class qlController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
