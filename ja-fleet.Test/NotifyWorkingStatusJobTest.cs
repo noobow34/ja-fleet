3a9decb2bf38f37c7f3143b53ec98137705011b8
Hiroaki KAWASHIMA <skywind.hk34@gmail.com>
2025-03-11 00:19:00 +0900

    結果通知を調整

---
+++ ja-fleet.Test/NotifyWorkingStatusJobTest.cs
@@ -0,0 +1,72 @@
+﻿using jafleet;
+using jafleet.Commons.Constants;
+using jafleet.Commons.EF;
+using jafleet.Jobs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Quartz;
+using Quartz.Impl;
+using System.Reflection.Metadata;
+
+namespace ja_fleet.Test
+{
+    [TestClass]
+    public sealed class NotifyWorkingStatusJobTest
+    {
+        [TestMethod]
+        public async Task ExecuteWithoutRefreshAsync()
+        {
+            var schedulerFactory = new StdSchedulerFactory();
+            var sch = await schedulerFactory.GetScheduler();
+            await sch.Start();
+
+            var jobDetail = JobBuilder.Create<NotifyWorkingStatusJob>()
+                .WithIdentity("NotifyWorkingStatusJob")
+                .Build();
+
+            var startDate = DateTime.Now.AddSeconds(10);
+
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("NotifyWorkingStatusJob")
+                .StartNow()
+                .WithCronSchedule($"{startDate.Second} {startDate.Minute} {startDate.Hour} ? * * *")
+                .Build();
+
+            await sch.ScheduleJob(jobDetail, trigger);
+
+            await Task.Delay(10 * 60 * 1000);
+        }
+
+        [TestMethod]
+        public async Task ExecuteWithtRefreshAsync()
+        {
+            var config = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory).AddJsonFile("appsettings.json").Build();
+            var options = new DbContextOptionsBuilder<JafleetContext>();
+            options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+            using JafleetContext jContext = new(options.Options);
+            var targetReg = jContext.AircraftViews.Where(a => a.OperationCode != OperationCode.RETIRE_UNREGISTERED).Take(50).AsNoTracking().ToArray().OrderBy(r => Guid.NewGuid());
+            var f = new RefreshWorkingStatusAndPhoto(targetReg, 15);
+            _ = f.ExecuteCheckAsync();
+
+            var schedulerFactory = new StdSchedulerFactory();
+            var sch = await schedulerFactory.GetScheduler();
+            await sch.Start();
+
+            var jobDetail = JobBuilder.Create<NotifyWorkingStatusJob>()
+                .WithIdentity("NotifyWorkingStatusJob")
+                .Build();
+
+            var startDate = DateTime.Now.AddSeconds(10);
+
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("NotifyWorkingStatusJob")
+                .StartNow()
+                .WithCronSchedule($"{startDate.Second} {startDate.Minute} {startDate.Hour} ? * * *")
+                .Build();
+
+            await sch.ScheduleJob(jobDetail, trigger);
+
+            await Task.Delay(10 * 60 * 1000);
+        }
+    }
+}
--- ja-fleet/Jobs/NotifyWorkingStatusJob.cs
+++ ja-fleet/Jobs/NotifyWorkingStatusJob.cs
@@ -16,9 +16,36 @@ namespace jafleet.Jobs
             var config = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory).AddJsonFile("appsettings.json").Build();
             Options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
             using JafleetContext jc = new (Options!.Options);
-            var log = jc.Logs.Where(l => l.LogType == LogType.WORKING_NOTIFY_TEXT).OrderByDescending(l => l.LogId).FirstOrDefault();
+            var log = jc.Logs.Where(l => l.LogType == LogType.WORKING_NOTIFY_TEXT && l.LogDate!.Value.Date == DateTime.Now.Date).OrderByDescending(l => l.LogId).FirstOrDefault();
             if (log != null) {
                 await SlackUtil.PostAsync(SlackChannelEnum.jafleet.GetStringValue(), log.LogDetail!);
+            }
+            else
+            {
+                if (RefreshWorkingStatusAndPhoto.Processing)
+                {
+                    await SlackUtil.PostAsync(SlackChannelEnum.jafleet.GetStringValue(), "RefreshWorkingStatusAndPhoto実行中です。終了したら通知します。");
+                    int waitCount = 0;
+                    while (RefreshWorkingStatusAndPhoto.Processing)
+                    {
+                        await Task.Delay(5 * 60 * 1_000);
+                        waitCount++;
+                        if (waitCount > 36)
+                        {
+                            await SlackUtil.PostAsync(SlackChannelEnum.jafleet.GetStringValue(), "RefreshWorkingStatusAndPhotoが完了しないため処理を中止します。");
+                            return;
+                        }
+                    }
+                    var log2 = jc.Logs.Where(l => l.LogType == LogType.WORKING_NOTIFY_TEXT && l.LogDate!.Value.Date == DateTime.Now.Date).OrderByDescending(l => l.LogId).FirstOrDefault();
+                    if (log2 != null)
+                    {
+                        await SlackUtil.PostAsync(SlackChannelEnum.jafleet.GetStringValue(), log2.LogDetail!);
+                    }
+                }
+                else
+                {
+                    await SlackUtil.PostAsync(SlackChannelEnum.jafleet.GetStringValue(), "本日はRefreshWorkingStatusAndPhotoが実行されていません。状況を確認してください。");
+                }
             }
         }
     }
